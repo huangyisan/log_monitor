5d74add782357f9413c52a14aab9ffa026c3d07f
huangyisan <anonymousyisan@gmail.com>
2018-11-19 20:30:25 +0800

    update

--- query.py
+++ query.py
@@ -1,18 +1,40 @@
 import requests
 import threading
-import queue
 from addict import Dict
-import auth_config
-import machine
 import time
+import json
+import base64
 
+import machine
+import auth_config
 
+class FuncLogin(object):
+    def __init__(self):
+        self.uri = 'http://10.1.11.23:9000/api/system/sessions'
+        self.headers = {
+            'Content-Type': 'application/json',
+            'Connection': 'keep-alive',
+        }
+        self.data = {
+            "username":"admin",
+            "password":"admin",
+            "host":"10.1.11.23:9000"
+        }
+
+    def get_session_id(self):
+        r = requests.post(url=self.uri, data=json.dumps(self.data), headers=self.headers)
+        return r.json().get('session_id',0)
+
+    def encode_base64(self):
+        s = self.get_session_id() + ':session'
+        code = base64.b64encode(s.encode())
+        return 'Basic ' + code.decode()
 
 class FuncQuery(object):
     def __init__(self, label):
         self.uri = 'http://10.1.11.23:9000/api/search/universal/relative'
         self.headers = {
-            'Authorization': auth_config.http_header.get('Authorization', 0),
+            'Authorization': FuncLogin().encode_base64(),
         }
         self.payloads = {
             'query': 'label:{0} | stats count(status)'.format(label),
@@ -48,15 +70,14 @@ def timer(func):
         print(cost_time)
     return warrp
 
-# @timer
-# def exec():
-for m in machine.machines:
-    c = threading.Thread(target=t, args=(m,))
+@timer
+def exec():
+    for m in machine.machines:
+        c = threading.Thread(target=t, args=(m,))
+        c.start()
+        t_list.append(c)
 
-    c.start()
-    t_list.append(c)
-
-for j in t_list:
-    j.join()
-# exec()
+    for j in t_list:
+        j.join()
+exec()
 print(zero_list)
