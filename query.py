6af947e67e16b615abf7e9764aea573629f961df
huangyisan <anonymousyisan@gmail.com>
2018-11-19 16:33:33 +0800

    update

--- query.py
+++ query.py
@@ -1,6 +1,10 @@
 import requests
+import threading
+import queue
 from addict import Dict
 import auth_config
+import machine
+
 
 headers = {
     'Authorization': auth_config.http_header.get('Authorization',0),
@@ -8,13 +12,17 @@ headers = {
 
 uri = 'http://10.1.11.23:9000/api/search/universal/relative'
 
-payload = {
-    'query':'label:ZheJiang-TZDX-L1-CO-CDN-2-HSM | stats count(status)',
-    'range':90,
-    'limit':1
-}
 
-r=requests.get(url=uri, params=payload, headers=headers)
-result_dict = r.json()
-mapping = Dict(result_dict)
-count = mapping.total_results
+for m in machine.machines:
+    payload = {
+        'query': 'label:{0} | stats count(status)'.format(m),
+        'range': 90,
+        'limit': 1
+    }
+    print(payload)
+
+    r=requests.get(url=uri, params=payload, headers=headers)
+    result_dict = r.json()
+    mapping = Dict(result_dict)
+    count = mapping.total_results
+    print(count)
